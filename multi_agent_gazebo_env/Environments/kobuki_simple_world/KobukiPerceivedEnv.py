e67d1a222d990f970cebffcbace361e023283ad0
abhay <abhayrawat2695@gmail.com>
2018-10-23 20:58:59 +0530

    redundant i/p removed

--- multi_agent_gazebo_env/Environments/kobuki_simple_world/KobukiPerceivedEnv.py
+++ multi_agent_gazebo_env/Environments/kobuki_simple_world/KobukiPerceivedEnv.py
@@ -31,25 +31,24 @@ class KobukiPerceivedEnv(object):
     self.tf_listener  = TransformListener(False, Duration(5.0))
     self.act_low      = np.array([-0.1, -np.pi/4])
     self.act_high     = np.array([0.25,  np.pi/4])
-    self.goal_idxs    = self.parent.goal_indices
+    self.goal_idxs    = self.parent.goal_idxs
     self.pause_sim    = self.parent.pause_sim
     self.unpause_sim  = self.parent.unpause_sim
     self.start_tf_broadcaster()
 
   def reset(self, pose, goals):
     # TODO handle reset of ros time?
-    self.unpause_sim()
     reset_state = ModelState()
     reset_state.model_name = self.model_name
     reset_state.pose.position.x = pose[0]
     reset_state.pose.position.y = pose[1]
     self.state_pub.publish(reset_state)
-    self.pause_sim()
     self.goal = np.hstack([goals[i] for i in self.neighbours])
     self.previous_act = [0., 0.]
     self.previous_obs = None
     print ("RESETTING AGENT {}".format(self._id))
     print ("GOAL PROVIDED: {}".format(self.goal))
+    return self.get_obs()
 
   def get_transforms(self):
     # the source and target frames have been changed here b/c
@@ -59,6 +58,10 @@ class KobukiPerceivedEnv(object):
     for nhbr in self.neighbours:
       source_frame = "{}_{}/base_link".format(self.model, self._id)
       target_frame = "{}_{}/base_link".format(self.model, nhbr)
+      tries = 0
+      print ("Waiting for transform b/w {} and {}".format(source_frame, target_frame))
+      self.tf_listener.waitForTransform(source_frame, target_frame, Time(), rospy.Duration(100))
+      print ("Found frame b/w {} and {}".format(source_frame, target_frame))
       Ptransform = self.tf_listener.lookupTransform(source_frame, target_frame, Time())
       state[nhbr] = Ptransform
     return state
@@ -68,25 +71,22 @@ class KobukiPerceivedEnv(object):
       self.previous_obs
     except:
       print("Can't call step before calling reset() ...")
-    self.unpause_sim()
     vel_cmd = Twist()
     vel_cmd.linear.x, vel_cmd.angular.z = action
     self.vel_pub.publish(vel_cmd)
     self.previous_act = action
-    self.pause_sim()
     return self.get_obs()
 
   def get_obs(self):
     # observation: [tx, ty , cos(tr), sin(tr), v_(t-1), w_(t-1)] * num_neighbours
-    self.unpause_sim()
     obs = []
     states = self.get_transforms()
-    self.pause_sim()
     for neighbour, state in states.items():
       translation = np.array(state[0])[:-1]
       rotation    = np.array(q2e(state[1])[-1]) # extract only YAW
       rotation_v  = np.array([np.cos(rotation), np.sin(rotation)]) # extract only YAW
-      obs.append(np.hstack([translation, rotation_v, self.previous_act]))
+      obs.append(np.hstack([translation, rotation_v]))
+    obs.append(self.previous_act)
     self.previous_obs = deepcopy(np.hstack(obs))
     obs = { "observation": self.previous_obs,
             "achieved_goal": self.previous_obs[self.goal_idxs],
