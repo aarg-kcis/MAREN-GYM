27d8eaf8f509f4c27dd21707847c7d5972c02cb5
aarg <abhayrawat2695@gmail.com>
2020-01-02 18:04:48 +0530

    minor changes in utils

--- multi_agent_gazebo_env/Scripts/Utils.py
+++ multi_agent_gazebo_env/Scripts/Utils.py
@@ -1,10 +1,10 @@
 import rospy
 
 def get_configuration(env_path):
-  try:
-    with open(os.path.join(env_path, "config.yaml")) as c_file:
-      config = yaml.load(c_file)
-      return config
-  except Exception as e:
-    print ("Problem in loading/parsing config file ...")
-    print ("Reading config file at: \n{}".format(env_path+"/config.yaml"))
+    try:
+        with open(os.path.join(env_path, "config.yaml")) as c_file:
+            config = yaml.load(c_file)
+        return config
+    except Exception as e:
+        print ("Problem in loading/parsing config file ...")
+        print ("Reading config file at: \n{}".format(env_path+"/config.yaml"))
